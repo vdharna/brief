afe88d0baf66c0ada18f4ed2b8e9bd4645e90d8e
vdharna <vdharna@MacBook-Pro-Retina.local>
2014-07-24 16:55:56 -0700

    Committing prior to significant compose view changes

--- Brief/ComposeBriefTableViewCell.swift
+++ Brief/ComposeBriefTableViewCell.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ComposeBriefTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    let redCellImage = UIImage(named: "compose-table-cell-red.png")
+    let greenCellImage = UIImage(named: "compose-table-cell-green.png")
+    
+    init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,6 +31,12 @@ class ComposeBriefTableViewCell: UITableViewCell {
         // Configure the view for the selected state
         cellLabel.numberOfLines = 5
         self.selectionStyle = UITableViewCellSelectionStyle.None
+        if (self.cellLabel.text.utf16Count > 140) {
+            self.cellImage.image = redCellImage
+        } else {
+            self.cellImage.image = greenCellImage
+            
+        }
     }
     
 }
