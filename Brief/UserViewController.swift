e333787ce598bd7fe642c413f41f4ee36b8aa5d9
vdharna <vdharna@MacBook-Pro-Retina.local>
2014-09-08 22:22:09 -0700

    cleanup

--- Brief/UserViewController.swift
+++ Brief/UserViewController.swift
@@ -63,43 +63,37 @@ class UserViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         
-       // dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-            
-            // Get picked image from info dictionary
-            var image = info[UIImagePickerControllerOriginalImage] as UIImage
-            
-            var newSize = CGSizeMake(512, 512);
-            
-            if (image.size.width > image.size.height) {
-                newSize.height = round(newSize.width * image.size.height / image.size.width);
-            } else {
-                newSize.width = round(newSize.height * image.size.width / image.size.height);
-            }
-            
-            UIGraphicsBeginImageContext(newSize)
-            image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
-            var data = UIImagePNGRepresentation(UIGraphicsGetImageFromCurrentImageContext())
-            UIGraphicsEndImageContext();
-
-            // write the image out to a cache file
-            var cachesDirectory = NSFileManager.defaultManager().URLForDirectory(NSSearchPathDirectory.CachesDirectory, inDomain: NSSearchPathDomainMask.UserDomainMask, appropriateForURL: nil, create: true, error: nil)
-            var temporaryName = NSUUID.UUID().UUIDString + ".png"
-            if let localURL = cachesDirectory?.URLByAppendingPathComponent(temporaryName) {
-                data.writeToURL(localURL, atomically: true)
-                self.cloudManager.uploadAssetWithURL(localURL, completionHandler: { record in
-                    
-                })
-            }
+        // Get picked image from info dictionary
+        var image = info[UIImagePickerControllerOriginalImage] as UIImage
         
-            // Put that image onto the screen in our image view
-            self.profileImageView.image = image
+        var newSize = CGSizeMake(256, 256);
         
-            
-            // dismiss the modal
-            self.dismissViewControllerAnimated(true, completion: nil)
-
-            
-      //  })
+        if (image.size.width > image.size.height) {
+            newSize.height = round(newSize.width * image.size.height / image.size.width);
+        } else {
+            newSize.width = round(newSize.height * image.size.width / image.size.height);
+        }
+        
+        UIGraphicsBeginImageContext(newSize)
+        image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
+        var data = UIImagePNGRepresentation(UIGraphicsGetImageFromCurrentImageContext())
+        UIGraphicsEndImageContext();
+        
+        // write the image out to a cache file
+        var cachesDirectory = NSFileManager.defaultManager().URLForDirectory(NSSearchPathDirectory.CachesDirectory, inDomain: NSSearchPathDomainMask.UserDomainMask, appropriateForURL: nil, create: true, error: nil)
+        var temporaryName = NSUUID.UUID().UUIDString + ".png"
+        if let localURL = cachesDirectory?.URLByAppendingPathComponent(temporaryName) {
+            data.writeToURL(localURL, atomically: true)
+            self.cloudManager.uploadAssetWithURL(localURL, completionHandler: { record in
+                
+            })
+        }
+        
+        // Put that image onto the screen in our image view
+        self.profileImageView.image = image
+        
+        // dismiss the modal
+        self.dismissViewControllerAnimated(true, completion: nil)
 
     }
     
